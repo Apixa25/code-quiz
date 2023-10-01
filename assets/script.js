7c632cd61e214343a9b5912a1ed2769d4f6d3433
stevensillsii <stevensills2@gmail.com>
2023-10-01 13:01:08 -0700

    defining in JS the question format

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// target start button
+const startButton = document.getElementById("start-btn");
+
+
+// all questions array
+const questions = [
+    {
+        text: "What is JavaScript?",
+        options: ["A way to write ancient scrolls", "Programming language used to add interactivity to web pages", "", "A type of font"],
+        answer: "Programming language used to add interactivity to web pages",
+    },
+
+    {
+        text: "what is the symbold for a constant?",
+        options: ["item", "const", "let", "var"],
+        answer: "const",
+    },
+    
